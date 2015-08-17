13e8d1d652c655a5021d3acca1b0a5af32cf9835
chenjunyu <842914439@qq.com>
2015-08-17 12:02:51 +0800

    support cleaning events
    support cleaning events

--- browser/browser-clean.js
+++ browser/browser-clean.js
@@ -325,15 +325,302 @@ module.exports = {
 }
 });define('/Users/ddchen/Coding/opensource/browser-clean/node_modules/rinse/index.js', function(require, module, exports){
 module.exports = require('/Users/ddchen/Coding/opensource/browser-clean/node_modules/rinse/lib/rinse.js');
+});define('/Users/ddchen/Coding/opensource/browser-clean/lib/objectRinser.js', function(require, module, exports){
+var rinse = require('/Users/ddchen/Coding/opensource/browser-clean/node_modules/rinse/index.js');
+var objectScenes = [];
+
+
+module.exports = {
+	record: function(confs) {
+		confs = confs || [];
+		objectScenes = [];
+		for (var i = 0; i < confs.length; i++) {
+			var conf = confs[i];
+			var objectScene = rinse.cover(conf.target, conf.opts);
+			objectScenes.push(objectScene);
+		}
+	},
+	clean: function() {
+		for (var i = 0; i < objectScenes.length; i++) {
+			var objectScene = objectScenes[i];
+			objectScene.recovery();
+		}
+	}
+}
+});define('/Users/ddchen/Coding/opensource/browser-clean/lib/originalEventRinser.js', function(require, module, exports){
+/**
+ * rinser for browser original events
+ * @author  ddchen
+ *
+ *  1. collect events
+ *  2. release events, consider white list
+ *
+ * eventMatrix
+ * 		[{
+ * 		   node : document,
+ * 		   events : [
+ * 		   		{
+ * 		   		   type: "click",
+ * 		   		   handlers: [ handler ]
+ * 		   		}
+ * 		   ]
+ * 		}]
+ *
+ *  proxyList
+ *      [{
+ *      	owner: owner,
+ *      	funName: funName
+ *      }]
+ *
+ * whiteList
+ *      [{
+ *      	node: node,   // must
+ *      	type: "click", // must
+ *      	handler: handler // must
+ *      }]
+ *
+ * to-do
+ *  1. Compatible IE
+ *
+ * represent: window and document
+ * In safari, window.hasOwnProperty("addEventListener") => true
+ */
+var whiteList = null;
+
+var eventMatrix = [];
+var proxyList = [];
+
+var globalObject = window;
+var domRepresent = globalObject.document;
+
+var proxyEventInterface = function() {
+	proxyAddEvent(globalObject);
+	proxyAddEvent(domRepresent);
+
+	proxyRemoveEvent(globalObject);
+	proxyRemoveEvent(domRepresent);
+}
+
+var clean = function() {
+	var list = getErgodicList();
+	for (var i = 0; i < list.length; i++) {
+		var item = list[i];
+		var node = item.node;
+		var type = item.type;
+		var handler = item.handler;
+		var funName = getRemoveName();
+		if (!containInWhiteList(node, type, handler)) {
+			node[funName].apply(node, [type, handler]);
+			removeEventInfoFromList(node, type, handler);
+		}
+	}
+}
+
+var proxyAddEvent = function(represent) {
+	var funName = getAddName();
+	proxyFun(represent, funName, function(owner, funName) {
+		var oldFun = owner[funName];
+		owner[funName] = function(eventType, eventHandler, useCapture) {
+			var node = this;
+			addEventInfoToList(node, eventType, eventHandler);
+			return oldFun.apply(this, [eventType, eventHandler, useCapture]);
+		}
+	});
+}
+
+var proxyRemoveEvent = function(represent) {
+	var funName = getRemoveName();
+	proxyFun(represent, funName, function(owner, funName) {
+		var oldFun = owner[funName];
+		owner[funName] = function(eventType, eventHandler, useCapture) {
+			var node = this;
+			removeEventInfoFromList(node, eventType, eventHandler);
+			return oldFun.apply(this, [eventType, eventHandler, useCapture]);
+		}
+	});
+}
+
+var proxyFun = function(represent, funName, callback) {
+	var owner = findFunOwner(represent, funName);
+	if (!owner || containInProxyList(owner, funName)) return;
+	callback && callback(owner, funName);
+	proxyList.push({
+		owner: owner,
+		funName: funName
+	});
+}
+
+var addEventInfoToList = function(node, eventType, eventHandler) {
+	if (!node || !eventType || !isFunction(eventHandler)) return;
+	var eventList = findEventList(node, true);
+	if (!eventList) return;
+	var eventTypeList = findEventTypeList(eventType, eventList, true);
+	if (!eventTypeList) return;
+	eventTypeList.handlers.push(eventHandler);
+}
+
+var removeEventInfoFromList = function(node, eventType, eventHandler) {
+	if (!node || !eventType || !isFunction(eventHandler)) return;
+	var eventList = findEventList(node, false);
+	if (!eventList) return;
+	var eventTypeList = findEventTypeList(eventType, eventList, false);
+	if (!eventTypeList) return;
+	var handlers = eventTypeList.handlers;
+	removeFromList(handlers, eventHandler);
+	// clear
+	if (handlers.length === 0) {
+		removeFromList(eventList.events, eventTypeList);
+	}
+	if (eventList.events.length === 0) {
+		removeFromList(eventMatrix, eventList);
+	}
+}
+
+var findEventTypeList = function(eventType, eventList, auto) {
+	var newEvt = null;
+	if (!eventType) return newEvt;
+	var events = eventList.events;
+	for (var i = 0; i < events.length; i++) {
+		var evt = events[i];
+		if (eventType === evt.type) {
+			return evt;
+		}
+	}
+	if (auto) {
+		newEvt = {
+			type: eventType,
+			handlers: []
+		}
+		events.push(newEvt);
+	}
+	return newEvt;
+}
+
+var findEventList = function(node, auto) {
+	var newEventList = null;
+	if (!node) return newEventList;
+	for (var i = 0; i < eventMatrix.length; i++) {
+		var eventList = eventMatrix[i];
+		if (eventList.node === node) {
+			return eventList;
+		}
+	}
+	if (auto) {
+		newEventList = {
+			node: node,
+			events: []
+		}
+		eventMatrix.push(newEventList);
+	}
+	return newEventList;
+}
+
+var containInProxyList = function(owner, funName) {
+	for (var i = 0; i < proxyList.length; i++) {
+		var proxyObj = proxyList[i];
+		if (proxyObj.owner === owner && proxyObj.funName === funName) {
+			return true;
+		}
+	}
+	return false;
+}
+
+var findFunOwner = function(startObj, funName) {
+	if (!startObj) return null;
+	var curObj = startObj;
+	while (curObj) {
+		if (curObj.hasOwnProperty(funName)) {
+			return curObj;
+		} else {
+			if (Object.getPrototypeOf) {
+				curObj = Object.getPrototypeOf(curObj);
+			} else {
+				curObj = curObj.__proto__;
+			}
+		}
+	}
+	return null;
+}
+
+var getAddName = function() {
+	return domRepresent.addEventListener ? "addEventListener" : "attachEvent";
+}
+
+var getRemoveName = function() {
+	return domRepresent.removeEventListener ? "removeEventListener" : "detachEvent";
+}
+
+var isFunction = function(fn) {
+	return !!fn && !fn.nodeName && fn.constructor != String &&
+		fn.constructor != RegExp && fn.constructor != Array &&
+		/function/i.test(fn + "");
+}
+
+var containInWhiteList = function(node, eventType, eventHandler) {
+	if (!whiteList) return false;
+	for (var i = 0; i < whiteList.length; i++) {
+		var whitePart = whiteList[i];
+		if (node === whitePart.node &&
+			eventType === whitePart.type &&
+			eventHandler === whitePart.handler) {
+			return true;
+		}
+	}
+	return false;
+}
+
+var getErgodicList = function() {
+	var list = [];
+	for (var i = 0; i < eventMatrix.length; i++) {
+		var eventList = eventMatrix[i];
+		var node = eventList.node;
+		var events = eventList.events;
+		for (var j = 0; j < events.length; j++) {
+			var eventTypeList = events[j];
+			var type = eventTypeList.type;
+			var handlers = eventTypeList.handlers;
+			for (var k = 0; k < handlers.length; k++) {
+				var handler = handlers[k];
+				list.push({
+					node: node,
+					type: type,
+					handler: handler,
+					eventList: eventList,
+					eventTypeList: eventTypeList,
+				});
+			}
+		}
+	}
+	return list;
+}
+
+var removeFromList = function(list, item) {
+	for (var i = 0; i < list.length; i++) {
+		if (list[i] === item) {
+			list.splice(i, 1);
+			i = i - 1;
+		}
+	}
+}
+
+module.exports = {
+	record: function(confs) {
+		confs = confs || {};
+		whiteList = confs.whiteList;
+		proxyEventInterface();
+	},
+	clean: clean
+}
 });define('/Users/ddchen/Coding/opensource/browser-clean/lib/browser-clean.js', function(require, module, exports){
 /**
  * @author ddchen
  */
 
-var rinse = require('/Users/ddchen/Coding/opensource/browser-clean/node_modules/rinse/index.js');
-var objectScenes = null;
+var objectRinser = require('/Users/ddchen/Coding/opensource/browser-clean/lib/objectRinser.js');
+var originalEventRinser = require('/Users/ddchen/Coding/opensource/browser-clean/lib/originalEventRinser.js');
 
 var cleanRules = [];
+var confs = null;
 
 var addCleanRule = function(rule) {
 	cleanRules.push(rule);
@@ -346,22 +633,19 @@ var isFunction = function(fn) {
 }
 
 addCleanRule(function() {
-	if (objectScenes) {
-		for (var i = 0; i < objectScenes.length; i++) {
-			var objectScene = objectScenes[i];
-			objectScene.recovery();
-		}
+	objectRinser.clean();
+	if (confs.events && confs.events.on === true) {
+		originalEventRinser.clean();
 	}
 });
 
 module.exports = {
 	addCleanRule: addCleanRule,
-	record: function(confs) {
-		objectScenes = [];
-		for (var i = 0; i < confs.length; i++) {
-			var conf = confs[i];
-			var objectScene = rinse.cover(conf.target, conf.opts);
-			objectScenes.push(objectScene);
+	record: function(opts) {
+		confs = opts || {};
+		objectRinser.record(confs.objects);
+		if (confs.events && confs.events.on === true) {
+			originalEventRinser.record(confs.events);
 		}
 	},
 	clean: function() {
@@ -371,7 +655,7 @@ module.exports = {
 		}
 	},
 	generalConf: {
-		windowIgnore: [/webkit/, "location", "document.location", "localStorage", "sessionStorage", "history", "browserCleaner"]
+		windowIgnore: [/webkit/, "location", "document.location", "localStorage", "name", "sessionStorage", "history"]
 	}
 }
 });define('/Users/ddchen/Coding/opensource/browser-clean/index.js', function(require, module, exports){
